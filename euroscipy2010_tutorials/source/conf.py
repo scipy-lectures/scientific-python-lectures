1935aee7edd1c11206cfb83a52f95d78b0403228
Emmanuelle Gouillart <emma@aleph.(none)>
2010-07-07 01:36:17 +0200

    added logo to the pdf

--- euroscipy2010_tutorials/source/conf.py
+++ euroscipy2010_tutorials/source/conf.py
@@ -187,7 +187,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'euroscipy_back.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
