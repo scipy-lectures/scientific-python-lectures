3302a73e01cd59b40cf97890540171a6c3b160b1
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-06-20 23:20:32 +0200

    Update release number

--- conf.py
+++ conf.py
@@ -62,7 +62,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u"Scipy lecture notes"
-copyright = u'2011'
+copyright = u'2012'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
