BUG: fix gradient descent examples

advanced/mathematical_optimization/examples/cost_functions.py
@@ -20,11 +20,13 @@ def gaussian_prime_prime(x):
 
 def mk_gauss(epsilon, ndim=2):
     def f(x):
+        x = np.asarray(x)
         y = x.copy()
         y *= np.power(epsilon, np.arange(ndim))
         return -gaussian(.5*y) + 1
 
     def f_prime(x):
+        x = np.asarray(x)
         y = x.copy()
         scaling = np.power(epsilon, np.arange(ndim))
         y *= scaling
@@ -32,6 +34,7 @@ def mk_gauss(epsilon, ndim=2):
 
     def hessian(x):
         epsilon = .07
+        x = np.asarray(x)
         y = x.copy()
         scaling = np.power(epsilon, np.arange(ndim))
         y *= .5*scaling
@@ -48,11 +51,13 @@ def mk_gauss(epsilon, ndim=2):
 
 def mk_quad(epsilon, ndim=2):
     def f(x):
+       x = np.asarray(x)
        y = x.copy()
        y *= np.power(epsilon, np.arange(ndim))
        return .33*np.sum(y**2)
 
     def f_prime(x):
+       x = np.asarray(x)
        y = x.copy()
        scaling = np.power(epsilon, np.arange(ndim))
        y *= scaling
@@ -132,7 +137,7 @@ class LoggingFunction(object):
         x_i, y_i = x0[:2]
         self.all_x_i.append(x_i)
         self.all_y_i.append(y_i)
-        f_i = self.function(x0)
+        f_i = self.function(np.asarray(x0))
         self.all_f_i.append(f_i)
         self.counter.append('f')
         self.counts.append(len(self.counter))

advanced/mathematical_optimization/examples/plot_gradient_descent.py
@@ -44,7 +44,7 @@ def gradient_descent(x0, f, f_prime, hessian=None, adaptative=False):
         all_x_i.append(x_i)
         all_y_i.append(y_i)
         all_f_i.append(f([x_i, y_i]))
-        dx_i, dy_i = f_prime([x_i, y_i])
+        dx_i, dy_i = f_prime(np.asarray([x_i, y_i]))
         if adaptative:
             # Compute a step size using a line_search to satisfy the Wolf
             # conditions
@@ -189,7 +189,7 @@ for index, ((f, f_prime, hessian), optimizer) in enumerate((
     pl.axes([0, 0, 1, 1])
 
     X = np.concatenate((x[np.newaxis, ...], y[np.newaxis, ...]), axis=0)
-    z = f([x, y])
+    z = np.apply_along_axis(f, 0, X)
     log_z = np.log(z + .01)
     pl.imshow(log_z,
             extent=[x_min, x_max, y_min, y_max],