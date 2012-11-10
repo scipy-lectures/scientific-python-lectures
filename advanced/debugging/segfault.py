9ec42325ff9ac5d8a46c1d65476f0a7fb9d81b95
Gael Varoquaux <gael.varoquaux@normalesup.org>
2012-11-10 14:31:50 +0100

    ENH: make sure that segfaults always happen
    Increase a lot the size of the overflow, to make sure that the memory
    lookup falls out of the program memory.

--- advanced/debugging/segfault.py
+++ advanced/debugging/segfault.py
@@ -1,9 +1,13 @@
+""" Simple code that creates a segfault using numpy. Used to learn
+debugging segfaults with GDB.
+"""
+
 import numpy as np
 from numpy.lib import stride_tricks
 
 def make_big_array(small_array):
-    big_array = stride_tricks.as_strided(small_array, 
-                                         shape=(20000, 20000), strides=(32, 32))
+    big_array = stride_tricks.as_strided(small_array,
+                                         shape=(2e6, 2e6), strides=(32, 32))
     return big_array
 
 def print_big_array(small_array):
@@ -16,4 +20,3 @@ l = list()
 for i in range(10):
     a = np.arange(8)
     l.append(print_big_array(a))
-    
