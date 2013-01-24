79a92f381640a9d355d79a3baf7be4f869cd3938
Valentin Haenel <valentin.haenel@gmx.de>
2013-01-24 12:10:30 +0100

    add the ctypes example

---
+++ advanced/interfacing_with_c/ctypes/cos_module.py
@@ -0,0 +1,14 @@
+import ctypes
+from ctypes.util import find_library
+
+# find and load the library
+libm = ctypes.cdll.LoadLibrary(find_library('m'))
+# set the argument type
+libm.cos.argtypes = [ctypes.c_double]
+# set the return type
+libm.cos.restype = ctypes.c_double
+
+
+def cos_func(arg):
+    ''' Wrapper for cos from math.h '''
+    return libm.cos(arg)
