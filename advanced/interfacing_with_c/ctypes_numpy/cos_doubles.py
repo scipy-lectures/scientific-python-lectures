666824485dee256eba01ae92970a01b19081374c
Valentin Haenel <valentin.haenel@gmx.de>
2013-02-02 00:02:20 +0100

    upgrade the documentation for the ctypes numpy documentation

--- advanced/interfacing_with_c/ctypes_numpy/cos_doubles.py
+++ advanced/interfacing_with_c/ctypes_numpy/cos_doubles.py
@@ -2,10 +2,16 @@ import numpy as np
 import numpy.ctypeslib as npct
 from ctypes import c_int
 
+# input type for the cos_doubles function
+# must be a double array, with single dimension that is contiguous
 array_1d_double = npct.ndpointer(dtype=np.double, ndim=1, flags='CONTIGUOUS')
 
+# load the library, using numpy mechanisms
 libcd = npct.load_library("libcos_doubles", ".")
+
+# setup the return typs and argument types
 libcd.cos_doubles.restype = None
 libcd.cos_doubles.argtypes = [array_1d_double, array_1d_double, c_int]
 
+# expose the function from the library to the top level in this module
 cos_doubles_func = libcd.cos_doubles
