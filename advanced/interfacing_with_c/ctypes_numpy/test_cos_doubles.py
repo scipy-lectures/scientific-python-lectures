cbe9c38bd2f955718907e0011f86178991e44bd0
Valentin Haenel <valentin.haenel@gmx.de>
2013-02-02 00:41:11 +0100

    some additional comments in the source code

--- advanced/interfacing_with_c/ctypes_numpy/test_cos_doubles.py
+++ advanced/interfacing_with_c/ctypes_numpy/test_cos_doubles.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pylab
+# this is the pure python ctypes wrapper, not the share library
 import cos_doubles
 
 x = np.arange(0, 2 * np.pi, 0.1)
-y = np.zeros(len(x))
+y = np.zeros(len(x))  # preallocate
 
 cos_doubles.cos_doubles_func(x, y, len(x))
 pylab.plot(x, y)
