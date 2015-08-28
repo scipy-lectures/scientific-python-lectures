4654346e11ac7b2be8bd451546280ec7e91f2a03
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2015-08-28 22:00:14 +0200

    Now uses a pure.css font styling

--- conf.py
+++ conf.py
@@ -188,7 +188,7 @@ html_title = "Scipy lecture notes"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['themes/scipy_lectures/static']
+html_static_path = ['themes/scipy_lectures/static', "_static"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
