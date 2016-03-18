917da27f38c8c672f0c16de4af0bcbfaf7f12d89
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-03-18 20:21:50 +0100

    Added (commented) case for windows

--- advanced/interfacing_with_c/ctypes/cos_module.py
+++ advanced/interfacing_with_c/ctypes/cos_module.py
@@ -1,14 +1,23 @@
 """ Example of wrapping cos function from math.h using ctypes. """
 
 import ctypes
-from ctypes.util import find_library
 
 # find and load the library
+
+# OSX or linux
+from ctypes.util import find_library
 libm = ctypes.cdll.LoadLibrary(find_library('m'))
+
+# Windows
+# from ctypes import windll
+# libm = cdll.msvcrt
+
+
 # set the argument type
 libm.cos.argtypes = [ctypes.c_double]
 # set the return type
 libm.cos.restype = ctypes.c_double
+
 
 
 def cos_func(arg):
