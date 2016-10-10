7b38c799be0a2b195837871fa42575471f682844
Gael Varoquaux <gael.varoquaux@normalesup.org>
2016-10-10 22:03:56 +0200

    WIP: start of rewamp of scikit-learn chapter

---
+++ packages/scikit-learn/examples/plot_iris_scatter.py
@@ -0,0 +1,31 @@
+"""
+Plot 2D views of the iris dataset
+=================================
+
+Plot a simple scatter plot of 2 features of the iris dataset.
+
+Note that more elaborate visualization of this dataset is detailed
+in the :ref:`statistics` chapter.
+"""
+
+# Load the data
+from sklearn.datasets import load_iris
+iris = load_iris()
+
+from matplotlib import pyplot as plt
+
+# The indices of the features that we are plotting
+x_index = 0
+y_index = 1
+
+# this formatter will label the colorbar with the correct target names
+formatter = plt.FuncFormatter(lambda i, *args: iris.target_names[int(i)])
+
+plt.figure(figsize=(5, 4))
+plt.scatter(iris.data[:, x_index], iris.data[:, y_index], c=iris.target)
+plt.colorbar(ticks=[0, 1, 2], format=formatter)
+plt.xlabel(iris.feature_names[x_index])
+plt.ylabel(iris.feature_names[y_index])
+
+plt.tight_layout()
+plt.show()
