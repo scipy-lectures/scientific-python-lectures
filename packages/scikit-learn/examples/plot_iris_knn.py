9668edd6f4895d42b7066bd53dd0c66921e9945d
Gael Varoquaux <gael.varoquaux@normalesup.org>
2016-10-12 16:16:12 +0200

    1 vs 3 nearest neighbors on iris prediction

--- packages/scikit-learn/examples/plot_iris_knn.py
+++ packages/scikit-learn/examples/plot_iris_knn.py
@@ -2,7 +2,8 @@
 Nearest-neighbor prediction on iris
 ====================================
 
-Plot the decision boundary of nearest neighbor decision on iris.
+Plot the decision boundary of nearest neighbor decision on iris, first
+with a single nearest neighbor, and then using 3 nearest neighbors.
 """
 
 import numpy as np
@@ -19,13 +20,34 @@ X = iris.data[:, :2]  # we only take the first two features. We could
                     # avoid this ugly slicing by using a two-dim dataset
 y = iris.target
 
-knn = neighbors.KNeighborsClassifier(n_neighbors=3)
+knn = neighbors.KNeighborsClassifier(n_neighbors=1)
 knn.fit(X, y)
 
 x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
 y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
 xx, yy = np.meshgrid(np.linspace(x_min, x_max, 100),
                         np.linspace(y_min, y_max, 100))
+Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
+
+
+###############################################################################
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+plt.figure()
+plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
+
+# Plot also the training points
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold)
+plt.xlabel('sepal length (cm)')
+plt.ylabel('sepal width (cm)')
+plt.axis('tight')
+
+
+###############################################################################
+# And now, redo the analysis with 3 neighbors
+knn = neighbors.KNeighborsClassifier(n_neighbors=3)
+knn.fit(X, y)
+
 Z = knn.predict(np.c_[xx.ravel(), yy.ravel()])
 
 # Put the result into a color plot
