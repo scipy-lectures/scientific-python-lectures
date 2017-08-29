9e3df7ef4841848903167d531b63584df4a06b50
Gael Varoquaux <gael.varoquaux@normalesup.org>
2017-08-29 10:45:52 +0200

    Make periodicity finder example more didactic

--- intro/scipy/examples/solutions/plot_periodicity_finder.py
+++ intro/scipy/examples/solutions/plot_periodicity_finder.py
@@ -3,24 +3,38 @@
 Crude periodicity finding
 ==========================
 
-Discover the periods in ../../data/populations.txt
+Discover the periods in evolution of animal populations
+(:download:`../../data/populations.txt`)
 """
-import numpy as np
-import matplotlib.pyplot as plt
 
+############################################################
+# Load the data
+############################################################
+
+import numpy as np
 data = np.loadtxt('../../../../data/populations.txt')
 years = data[:, 0]
 populations = data[:, 1:]
 
-ft_populations = np.fft.fft(populations, axis=0)
-frequencies = np.fft.fftfreq(populations.shape[0], years[1] - years[0])
-periods = 1 / frequencies
+############################################################
+# Plot the data
+############################################################
 
+import matplotlib.pyplot as plt
 plt.figure()
 plt.plot(years, populations * 1e-3)
 plt.xlabel('Year')
 plt.ylabel('Population number ($\cdot10^3$)')
 plt.legend(['hare', 'lynx', 'carrot'], loc=1)
+
+############################################################
+# Plot its periods
+############################################################
+from scipy import fftpack
+
+ft_populations = fftpack.fft(populations, axis=0)
+frequencies = fftpack.fftfreq(populations.shape[0], years[1] - years[0])
+periods = 1 / frequencies
 
 plt.figure()
 plt.plot(periods, abs(ft_populations) * 1e-3, 'o')
@@ -30,6 +44,7 @@ plt.ylabel('Power ($\cdot10^3$)')
 
 plt.show()
 
+############################################################
 # There's probably a period of around 10 years (obvious from the
 # plot), but for this crude a method, there's not enough data to say
 # much more.
