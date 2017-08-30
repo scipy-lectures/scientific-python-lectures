d476df63fb8c8efa5c7604ed3423588071e85d1a
Gael Varoquaux <gael.varoquaux@normalesup.org>
2017-08-30 23:42:14 +0200

    More connect -> connected

--- intro/scipy/examples/plot_connect_measurements.py
+++ intro/scipy/examples/plot_connect_measurements.py
@@ -1,9 +1,9 @@
 """
 =============================
-Demo connect components
+Demo connected components
 =============================
 
-Extracting and labeling connect components in a 2D array
+Extracting and labeling connected components in a 2D array
 """
 
 import numpy as np
@@ -30,7 +30,7 @@ plt.subplots_adjust(wspace=.05, left=.01, bottom=.01, right=.99, top=.9)
 
 
 ############################################################
-# Label connect components
+# Label connected components
 from scipy import ndimage
 labels, nb = ndimage.label(mask)
 
@@ -43,11 +43,11 @@ plt.subplots_adjust(wspace=.05, left=.01, bottom=.01, right=.99, top=.9)
 
 
 ############################################################
-# Extract the 4th connect component, and crop the array around it
+# Extract the 4th connected component, and crop the array around it
 sl = ndimage.find_objects(labels==4)
 plt.figure(figsize=(3.5, 3.5))
 plt.imshow(sig[sl[0]])
-plt.title('Cropped connect component')
+plt.title('Cropped connected component')
 plt.axis('off')
 
 plt.subplots_adjust(wspace=.05, left=.01, bottom=.01, right=.99, top=.9)
