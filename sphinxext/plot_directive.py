b935733d99bbe0f45210420d14a07da0efe02547
emmanuelle <emmanuelle.gouillart@nsup.org>
2017-08-30 23:19:56 +0200

    Hacked plot directive for python3 compatibility

--- sphinxext/plot_directive.py
+++ sphinxext/plot_directive.py
@@ -16,7 +16,11 @@ The set of file formats to generate can be specified with the
 plot_formats configuration variable.
 """
 
-import sys, os, glob, shutil, hashlib, imp, warnings, cStringIO
+import sys, os, glob, shutil, hashlib, imp, warnings
+try:
+        from cStringIO import StringIO
+except ImportError:
+        from io import StringIO
 import traceback
 import re
 try:
@@ -59,22 +63,22 @@ else:
         """
 
         if not os.path.exists(target):
-            raise OSError, 'Target does not exist: '+target
+            raise(OSError, 'Target does not exist: '+target)
 
         if not os.path.isdir(base):
-            raise OSError, 'Base is not a directory or does not exist: '+base
+            raise(OSError, 'Base is not a directory or does not exist: '+base)
 
         base_list = (os.path.abspath(base)).split(os.sep)
         target_list = (os.path.abspath(target)).split(os.sep)
 
         # On the windows platform the target may be on a completely different drive from the base.
-        if os.name in ['nt','dos','os2'] and base_list[0] <> target_list[0]:
-            raise OSError, 'Target is on a different drive to base. Target: '+target_list[0].upper()+', base: '+base_list[0].upper()
+        if os.name in ['nt','dos','os2'] and base_list[0] != target_list[0]:
+            raise( OSError, 'Target is on a different drive to base. Target: '+target_list[0].upper()+', base: '+base_list[0].upper())
 
         # Starting from the filepath root, work out how much of the filepath is
         # shared by base and target.
         for i in range(min(len(base_list), len(target_list))):
-            if base_list[i] <> target_list[i]: break
+            if base_list[i] != target_list[i]: break
         else:
             # If we broke out of the loop, i is pointing to the first differing path elements.
             # If we didn't break out of the loop, i is pointing to identical path elements.
@@ -140,7 +144,7 @@ def runfile(fullpath):
     path, fname = os.path.split(fullpath)
     sys.path.insert(0, os.path.abspath(path))
     stdout = sys.stdout
-    sys.stdout = cStringIO.StringIO()
+    sys.stdout = StringIO()
     os.chdir(path)
     try:
         fd = open(fname)
