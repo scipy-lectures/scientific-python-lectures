9a61b30f0d7021d02b4f313043403ac3bf21ef0a
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2017-09-01 12:34:53 +0200

    two plots added

---
+++ intro/numpy/examples/plot_basic2dplot.py
@@ -0,0 +1,14 @@
+"""
+2D plotting
+===========
+
+Plot a basic 2D figure
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = np.random.rand(30, 30)
+plt.imshow(image, cmap=plt.cm.hot)
+plt.colorbar()
+plt.show()
