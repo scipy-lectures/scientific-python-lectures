913f12e67de9df0e1baf6dacfa087c6da7b694c1
Gael Varoquaux <gael.varoquaux@normalesup.org>
2018-09-01 17:10:17 +0200

    Try to fix links

--- conf.py
+++ conf.py
@@ -73,7 +73,6 @@ sphinx_gallery_conf = {
                      'guide/auto_examples',
                      ],
     'doc_module': 'scipy-lecture-notes',
-    'backreferences_dir': False,
     }
 
 # Add any paths that contain templates here, relative to this directory.
