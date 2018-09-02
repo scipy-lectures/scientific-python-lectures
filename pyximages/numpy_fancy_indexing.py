e4991cc95a8201d8c245465082f2cda8c7f8a744
Gert-Ludwig Ingold <gert.ingold@physik.uni-augsburg.de>
2018-09-02 17:02:23 +0200

    made code consistent with numpy_indexing image

--- pyximages/numpy_fancy_indexing.py
+++ pyximages/numpy_fancy_indexing.py
@@ -11,17 +11,36 @@ from math import cos, radians, sin
 from pyx import canvas, color, path, text, unit
 
 
-def markbox(x, y, boxcolor, linewidthfactor=0.1, boxsize=1):
-    '''mark box by a colored line
+def framebox(nx, ny, framecolor, nw=1, nh=1, boxsize=1, framefactor=0.1,
+             reducedsize=False):
+    '''draw color frame in one or across several boxes
 
-       the drawing is done on the global canvas called c
+       The drawing is done on the global canvas called c.
+
+       nx, ny:      coordinates of lower left corner in number of boxes
+       framecolor:  color
+       nw, nh:      width and height of frame in boxes
+       framefactor: framewidth as fraction of boxsize
+       boxsize:     side length of fox
+       reducedsize: if True, the size of the frame is reduced by the framewidth
+
     '''
-    linewidth = linewidthfactor*boxsize
-    p = (path.rect(x, y, boxsize, boxsize)
-         + path.rect(x+linewidth, y+linewidth,
-                     boxsize-2*linewidth, boxsize-2*linewidth).reversed()
+    x = nx*boxsize
+    y = ny*boxsize
+    w = nw*boxsize
+    h = nh*boxsize
+    if reducedsize:
+        outer_offset = framefactor*boxsize
+        inner_offset = 2*outer_offset
+    else:
+        outer_offset = 0
+        inner_offset = framefactor*boxsize
+    p = (path.rect(x+outer_offset, y+outer_offset,
+                   w-2*outer_offset, h-2*outer_offset)
+         + path.rect(x+inner_offset, y+inner_offset,
+                     w-2*inner_offset, h-2*inner_offset).reversed()
          )
-    c.fill(p, [boxcolor])
+    c.fill(p, [framecolor])
 
 
 ncols = 6
@@ -30,37 +49,28 @@ boxsize = 1
 angle = radians(40)
 reducedboxsize = 0.65*boxsize
 
+ex1color = color.rgb(0, 0.5, 0)
+ex2color = color.rgb(0, 0, 0.7)
+ex3color = color.rgb(0.7, 0, 0)
+
 text.set(text.LatexRunner)
-text.preamble(r'''\usepackage[T1]{fontenc}
-                  \usepackage{bera}
-                  \renewcommand*\familydefault{\ttdefault}
-                  \usepackage{color}
-                  \definecolor{ex1}{rgb}{0, 0, 0.7}
-                  \definecolor{ex2}{rgb}{0, 0.6, 0}
-                  \definecolor{ex3}{rgb}{0.7, 0, 0}''')
+preamble = r'''\usepackage[T1]{fontenc}
+               \usepackage{bera}
+               \renewcommand*\familydefault{\ttdefault}
+               \usepackage{color}'''
+for nr, elem in enumerate((ex1color, ex2color, ex3color)):
+    preamble = preamble+r'\definecolor{{ex{}color}}{{rgb}}{{{}, {}, {}}}'.format(
+                  nr+1, elem.r, elem.g, elem.b)
+text.preamble(preamble)
 unit.set(xscale=1.2)
 
 c = canvas.canvas()
-
-ex1color = color.rgb(0, 0, 0.7)
-linewidth = 0.1*boxsize
-for n in (0, 2, 5):
-    c.fill(path.rect(n*boxsize+linewidth, linewidth,
-                     boxsize-2*linewidth, 3*boxsize-2*linewidth),
-           [ex1color])
-    c.fill(path.rect(n*boxsize+2*linewidth, 2*linewidth,
-                     boxsize-4*linewidth, 3*boxsize-4*linewidth),
-           [color.grey(1)])
-
-ex2color = color.rgb(0, 0.6, 0)
-linewidth = 0.1
 for n in range(5):
-    markbox((n+1)*boxsize, (ncols-n-1)*boxsize, ex2color)
-
-ex3color = color.rgb(0.7, 0, 0)
-linewidth = 0.1
+    framebox(n+1, ncols-n-1, ex1color)
+for nx in (0, 2, 5):
+    framebox(nx, 0, ex2color, nh=3, reducedsize=True)
 for n in (0, 2, 5):
-    markbox(2*boxsize, (ncols-n-1)*boxsize, ex3color)
+    framebox(2, (ncols-n-1), ex3color)
 
 for nx in range(ncols+1):
     p = path.path(path.moveto(nx*boxsize, 0),
@@ -78,30 +88,43 @@ p = path.path(path.moveto(ncols*boxsize+reducedboxsize*cos(angle),
               path.rlineto(-nrows*boxsize, 0),
               path.rlineto(-reducedboxsize*cos(angle), -reducedboxsize*sin(angle)))
 c.stroke(p)
+
 for nx in range(ncols):
     x = (nx+0.5)*boxsize
     for ny in range(nrows):
         y = (ncols-ny-0.5)*boxsize
-        c.text(x, y, r'\textbf{{{}}}'.format(ny*10+nx), [text.halign.center, text.valign.middle])
+        c.text(x, y, r'\textbf{{{}}}'.format(ny*10+nx),
+               [text.halign.center, text.valign.middle])
 
 parwidth = 10.6
-s = r'''\noindent\textcolor{ex2}{\bfseries>{}>{}> a[(0,1,2,3,4), (1,2,3,4,5)]}\\[0.1\baselineskip]
-        \textcolor{ex2}{array([1, 12, 23, 34, 45])}
-     '''
-c.text(-parwidth-0.5, ncols*boxsize, s, [text.valign.top, text.parbox(parwidth)])
+inputtemplate = r'\noindent\textcolor{{ex{}color}}{{\bfseries>{{}}>{{}}> {}}}'
+resulttemplate = r'\textcolor{{ex{}color}}{{{}}}'
+myattrs = [text.valign.top, text.parbox(parwidth)]
+textblockdist = 0.5
+ytop = ncols*boxsize
 
-s = r'''\noindent\textcolor{ex1}{{\bfseries>{}>{}> a[3:, [0,2,5]]}\\[0.1\baselineskip]
-        \textcolor{ex1}{array([[30, 32, 35],}\\
-        \textcolor{ex1}{\hphantom{array([}[40, 42, 45],}\\
-        \textcolor{ex1}{\hphantom{array([}[50, 52, 55]])}}
-     '''
-c.text(-parwidth-0.5, ncols*boxsize-1.5, s, [text.valign.top, text.parbox(parwidth)])
+for s in ((inputtemplate.format('1', 'a[(0,1,2,3,4), (1,2,3,4,5)]')
+           + r'\\[0.1\baselineskip]'
+           + resulttemplate.format('1', r'array([1, 12, 23, 34, 45])')
+           ),
+          (inputtemplate.format('2', 'a[3:, [0,2,5]]')
+           + r'\\[0.1\baselineskip]'
+           + resulttemplate.format('2', 'array([[30, 32, 35],')
+           + r'\\'
+           + resulttemplate.format('2', r'\hphantom{array([}[40, 42, 45],')
+           + r'\\'
+           + resulttemplate.format('2', r'\hphantom{array([}[50, 52, 55]])')
+           ),
+          (inputtemplate.format('3', 'mask = np.array([1,0,1,0,0,1], dtype=bool)')
+           + r'\\'
+           + inputtemplate.format('3', 'a[mask, 2]')
+           + r'\\[0.1\baselineskip]'
+           + resulttemplate.format('3', 'array([2, 22, 52])')
+           )):
+    t = text.text(-parwidth-0.5, ytop, s, myattrs)
+    c.insert(t)
+    ytop = ytop-t.bbox().height()-textblockdist
 
-s = r'''\noindent\textcolor{ex3}{\bfseries>{}>{}> mask = np.array([1,0,1,0,0,1], dtype=bool)}\\
-        \textcolor{ex3}{\bfseries>{}>{}> a[mask, 2]}\\[0.1\baselineskip]
-        \textcolor{ex3}{array([2, 22, 52])}
-     '''
-c.text(-parwidth-0.5, ncols*boxsize-4, s, [text.valign.top, text.parbox(parwidth)])
 
 basename = os.path.splitext(sys.argv[0])[0]
 c.writeGSfile(basename+'.png', resolution=150)
