0aa4b430ec2b4bfc7e2d25d6aa74755bfad60dde
Sebastian Berg <sebastian@sipsolutions.net>
2019-02-08 18:27:36 +0100

    ENH: Make the numpy advanced iterator example more elaborate
    This uses the iterator to allocate the output. It also adds some
    checks so that the code will not fail on non-native byte order
    or unalinged inputs.
    It also makes use of the inner loop optimization which should be
    used for most iterations such as here.
    
    Possibly this goes too far and a simpler version is better?
    
    Closes gh-402.

--- advanced/interfacing_with_c/numpy_c_api/test_cos_module_np.py
+++ advanced/interfacing_with_c/numpy_c_api/test_cos_module_np.py
@@ -6,3 +6,35 @@ x = np.arange(0, 2 * np.pi, 0.1)
 y = cos_module_np.cos_func_np(x)
 plt.plot(x, y)
 plt.show()
+
+
+# Below are more specific tests for less common usage
+# ---------------------------------------------------
+
+# The function is OK with `x` not having any elements:
+x_empty = np.array([], dtype=np.float64)
+y_empty = cos_module_np.cos_func_np(x_empty)
+assert np.array_equal(y_empty, np.array([], dtype=np.float64))
+
+# The function can handle arbitrary dimensions and non-contiguous data.
+# `x_2d` contains the same values, but has a different shape.
+# Note: `x_2d.flags` shows it is not contiguous and `x2.ravel() == x`
+x_2d = x.repeat(2)[::2].reshape(-1, 3)
+y_2d = cos_module_np.cos_func_np(x_2d)
+# When reshaped back, the same result is given:
+assert np.array_equal(y_2d.ravel(), y)
+
+# The function handles incorrect byte-order fine:
+x_not_native_byteorder = x.astype(x.dtype.newbyteorder())
+y_not_native_byteorder = cos_module_np.cos_func_np(x_not_native_byteorder)
+assert np.array_equal(y_not_native_byteorder, y)
+
+# The function fails if the data type is incorrect:
+x_incorrect_dtype = x.astype(np.float32)
+try:
+    cos_module_np.cos_func_np(x_incorrect_dtype)
+    assert 0, "This cannot be reached."
+except TypeError:
+    # A TypeError will be raised, this can be changed by changing the
+    # casting rule.
+    pass
