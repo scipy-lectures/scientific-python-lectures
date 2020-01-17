234b0af65739ad9279ef039caf935ba82dc4d988
Pierre de Buyl <pdebuyl@pdebuyl.be>
2020-01-17 22:59:57 +0100

    change gaussian_filter to gaussian

--- packages/scikit-image/examples/plot_labels.py
+++ packages/scikit-image/examples/plot_labels.py
@@ -17,7 +17,7 @@ np.random.seed(1)
 im = np.zeros((l, l))
 points = l * np.random.random((2, n ** 2))
 im[(points[0]).astype(np.int), (points[1]).astype(np.int)] = 1
-im = filters.gaussian_filter(im, sigma= l / (4. * n))
+im = filters.gaussian(im, sigma= l / (4. * n))
 blobs = im > 0.7 * im.mean()
 
 all_labels = measure.label(blobs)
