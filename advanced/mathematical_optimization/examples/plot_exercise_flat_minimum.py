c8372e5323ce0cf3dd409ac74f8757ea880d13e2
Stefan van der Walt <stefanv@berkeley.edu>
2024-09-18 17:30:05 -0700

    Install matplotlib types for pre-commit-mypy

--- advanced/mathematical_optimization/examples/plot_exercise_flat_minimum.py
+++ advanced/mathematical_optimization/examples/plot_exercise_flat_minimum.py
@@ -49,9 +49,7 @@ plt.plot(t, f([0, t]))
 plt.figure(1)
 plt.clf()
 X, Y = np.mgrid[-1.5:1.5:100j, -1.1:1.1:100j]  # type: ignore[misc]
-plt.imshow(
-    f([X, Y]).T, cmap="gray_r", extent=(-1.5, 1.5, -1.1, 1.1), origin="lower"
-)
+plt.imshow(f([X, Y]).T, cmap="gray_r", extent=(-1.5, 1.5, -1.1, 1.1), origin="lower")
 plt.contour(X, Y, f([X, Y]), cmap="gnuplot")
 
 # Plot the gradient
